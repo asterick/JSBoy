bec513b343e424c543c4cb414e7dea712d10e721
Bryon Vandiver <unicdk@gmail.com>
2012-09-10 10:11:48 -0700

    Added missing third party libraries, improved audio framework

--- src/chips/audio.js
+++ src/chips/audio.js
@@ -1,5 +1,35 @@
 define([], function () {
+    var BUFFER_LENGTH = 2048;
+
     function Audio() {
+        if (this.context) {
+            this.node = this.context.createJavaScriptNode(BUFFER_LENGTH);
+            this.node.onaudioprocess = this.$('process');
+        }
+    }
+
+    // Don't assume audio is available, 
+    Audio.prototype.context = 
+        window.webkitAudioContext && (new webkitAudioContext());
+
+    Audio.prototype.mute = function () {
+        if (!this.node) { return ; }
+
+        this.node.disconnect();
+    };
+    
+    Audio.prototype.play = function () {
+        if (!this.node) { return ; }
+
+        this.node.connect(this.context.destination);
+    };
+
+    Audio.prototype.process = function (e) {
+        var left = e.outputBuffer.getChannelData(0),
+            right = e.outputBuffer.getChannelData(1),
+            length = left.length;
+
+        for (var i = 0; i < length; i++) { left[i] = right[i] = 0; }
     }
 
     Audio.prototype.clock = function (ticks) {
